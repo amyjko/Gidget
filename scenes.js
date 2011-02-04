cd87257988311381311624be5c93d6f8537d9943
Andy <andyjko@gmail.com>
2011-02-04 13:50:56 -0800

    First commit of scenes functionality.

---
+++ scenes.js
@@ -0,0 +1,32 @@
+var SCENES = {
+
+	this.Scene = function() {
+	
+		this.length = 1000;
+	
+		this.setLength = function(ms) {
+		
+		
+		
+		};
+	
+	};
+
+	this.Movie = function() {
+	
+		this.scenes = [];
+		
+		this.addScene = function(scene) {
+		
+			scenes.push(scene);
+		
+		};
+	
+	};
+
+
+
+
+
+
+}
