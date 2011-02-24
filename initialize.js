e0a58b994dbd24d7e1be242f7faa4a572a82f444
mjslee <mikaly@gmail.com>
2011-02-24 05:56:51 -0800

    89 - replaced browser tooltips with jQuery tooltips (CSS styled divs)

--- initialize.js
+++ initialize.js
@@ -226,6 +226,39 @@ $().ready(function() {
 
 	GIDGET.ui.updateBonus();
 	
+	
+	//Targets a tag using jQuery and adding a div to it. It extracts the 'title' tag from the code and uses that as its text.
+	function tooltip(target_items, name) {
+		$(target_items).each(function(i) {
+			$("body").append("<div class='"+name+"' id='"+name+i+"'><p>"+$(this).attr('title')+"</p></div>");
+			
+			var my_tooltip = $("#"+name+i);
+
+			if($(this).attr("title") != "") { // checks if there is a title
+
+				$(this).removeAttr("title").mouseover(function() {
+					my_tooltip.css({opacity:0.8, display:"none"}).fadeIn(400);
+				}).mousemove(function(kmouse) {
+					my_tooltip.css({left:kmouse.pageX+15, top:kmouse.pageY+15});
+				}).mouseout(function(){
+					my_tooltip.fadeOut(400);
+				});
+
+			}
+		});
+	}
+
+	// State which tags and which style we'd like to apply to.
+	$(document).ready(function() {
+		 tooltip("h2","tooltip");
+		 tooltip("h3","tooltip");
+		 //tooltip("button","tooltip");
+		 //tooltip("canvas","tooltip");
+		 tooltip("span","tooltip");
+	});
+	
+	
+	
 	// Pre-load sound effects
 	GIDGET.sfx.load();
 
