89060d737a445dda1bcfd564d01d6d8d8fef423d
Andy <andyjko@gmail.com>
2011-03-11 13:22:00 -0800

    Fixed wrong argument in editingDisabled()

--- text.js
+++ text.js
@@ -378,7 +378,7 @@ GIDGET.text = {
 	editingDisabled: function() {
 	
 		if (GIDGET.experiment.isControl())
-			return new GIDGET.text.Message("ERROR: Cannot modify code during program execution. Click the <b> to " + this.finishExecutingButtonLabel() + "</b> button to end execution and then on  <b>retry this misssion</b> button to restart.", "error", undefined, GIDGET.text.SAD);
+			return new GIDGET.text.Message("ERROR: Cannot modify code during program execution. Click the <b> to " + this.finishExecutingButtonLabel() + "</b> button to end execution and then on  <b>retry this misssion</b> button to restart.", "error", GIDGET.text.SAD);
 	
 		return new GIDGET.text.Message("If you change my instructions while I'm doing them, I'm going to get really confused! You can make me stop by pressing <b> to " + this.finishExecutingButtonLabel() + "</b> button and then clicking the <b>retry this mission</b> button.", "errorExp", GIDGET.text.SAD);
 		
