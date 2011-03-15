a7dbe62805f31a198e6fe4c2fcd362dd06b5f4ac
mjslee <mikaly@gmail.com>
2011-03-15 08:16:04 -0700

    added browser checking

--- initialize.js
+++ initialize.js
@@ -20,6 +20,90 @@ function setLocalStorageObject(key, value) {
 
 $().ready(function() {
 
+	// Add browser detection, including Chrome
+	var BrowserDetect = {
+		init: function () {
+			this.browser = this.searchString(this.dataBrowser) || "unknown";
+			this.version = this.searchVersion(navigator.userAgent)
+				|| this.searchVersion(navigator.appVersion)
+				|| "an unknown version";
+			this.OS = this.searchString(this.dataOS) || "unknown";
+		},
+		searchString: function (data) {
+			for (var i=0;i<data.length;i++)	{
+				var dataString = data[i].string;
+				var dataProp = data[i].prop;
+				this.versionSearchString = data[i].versionSearch || data[i].identity;
+				if (dataString) {
+					if (dataString.indexOf(data[i].subString) != -1)
+						return data[i].identity;
+				}
+				else if (dataProp)
+					return data[i].identity;
+			}
+		},
+		searchVersion: function (dataString) {
+			var index = dataString.indexOf(this.versionSearchString);
+			if (index == -1) return;
+			return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+		},
+		dataBrowser: [
+			{
+				string: navigator.userAgent,
+				subString: "Chrome",
+				identity: "Chrome"
+			},
+			{
+				string: navigator.vendor,
+				subString: "Apple",
+				identity: "Safari",
+				versionSearch: "Version"
+			},
+			{
+				prop: window.opera,
+				identity: "Opera"
+			},
+			{
+				string: navigator.userAgent,
+				subString: "Firefox",
+				identity: "Firefox"
+			},
+			{
+				string: navigator.userAgent,
+					subString: "MSIE",
+				identity: "Explorer",
+				versionSearch: "MSIE"
+			}
+		],
+		dataOS : [
+			{
+				string: navigator.platform,
+				subString: "Win",
+				identity: "Windows"
+			},
+			{
+				string: navigator.platform,
+				subString: "Mac",
+				identity: "Mac"
+			},
+			{
+				string: navigator.platform,
+				subString: "Linux",
+				identity: "Linux"
+			}
+		]
+
+	};
+
+	// Check browser support, redirect if not supported	
+	function checkBrowser() {
+		BrowserDetect.init();
+		if ((BrowserDetect.browser == "Opera" || BrowserDetect.browser == "MSIE") || (BrowserDetect.browser == "Chrome" && BrowserDetect.OS == "Mac")) window.location.href = "unsupported.html";
+		// Commented out for now until we ger results for issue #195
+		//else if (BrowserDetect.browser == "Safari" && BrowserDetect.OS == "Windows") window.location.href = "unsupported.html";
+	}
+	checkBrowser();
+
 	function supportsCanvas() {
 	
 		return !!document.createElement('canvas').getContext;
